33cb4192148689cb2d0866b9b8299651f86d4db1
Mercymuhonja <muhonjamercy63@gmail.com>
2019-04-30 19:55:20 +0300

    Math operations and classes

---
+++ shapes.py
@@ -0,0 +1,58 @@
+class Circle:
+	def __init__(self,r):
+		self.radius=r
+
+	def area(self):
+	    return self.radius 3.142*2
+
+	def circumference(self):
+		return r*self.perimeter*3.142
+
+class Square:
+	def__init__(self,a):
+	    self.side=a
+
+	def Area(self):
+		return self.area 2(a*b)
+	def perimeter(self)
+		return self.perimeter 2(a+b)
+
+class Rectangle:
+	def__init__(self,w,l):
+	    self.length=l
+	    self.width=w
+
+	def area(self):
+		return self.length*self.width
+	def perimeter(self)
+		return self.2(length*width)
+
+class Sphere:
+    def __init__(self, r):
+        self.radius = radius
+        self.area = 0
+        self.volume = 0
+        
+    
+    def SurfaceArea(self):
+        r = self.radius
+        self.area = 4 * 3.14 * (r*r)
+        return (self.area)
+    
+    def Volume(self):
+        r = self.radius
+        self.volume = (4/3) * 3.14 * (r * r * r)
+        return (self.volume)
+
+     
+
+	
+
+
+
+
+
+
+
+
+
